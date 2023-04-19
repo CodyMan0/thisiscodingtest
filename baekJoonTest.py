fbca4e3416d873ef2b9e353090599adb32acc300
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-04-19 14:36:32 +0900

    add: 4.19 구현 문제 풀이

--- baekJoonTest.py
+++ baekJoonTest.py
@@ -1,6 +1,11 @@
+from sys import stdin
 
-n = int(input())
-data = [input() for i in range(n)]
-
-for i in data:
-    print(i.lower())
+for _ in range(3):
+    N = int(stdin.readline())
+    list = [int(stdin.readline()) for i in range(N)]
+    if sum(list) == 0:
+        print("0")
+    elif sum(list) > 0:
+        print("+")
+    else:
+        print("-")
