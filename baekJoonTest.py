f86e58beeb45008ccd6b7d098a978c73356e989a
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-04-19 15:18:34 +0900

    add: 4.19 DFS&BFS 개념 복습

--- baekJoonTest.py
+++ baekJoonTest.py
@@ -1,11 +1,29 @@
-from sys import stdin
+from collections import deque
 
-for _ in range(3):
-    N = int(stdin.readline())
-    list = [int(stdin.readline()) for i in range(N)]
-    if sum(list) == 0:
-        print("0")
-    elif sum(list) > 0:
-        print("+")
-    else:
-        print("-")
+def bfs(graph,start,visited):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end =' ')
+        for i in graph[v] :
+            if not visited[i] :
+                queue.append(i)
+                visited[i] = True
+
+
+graph = [
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+
+visited = [False] * 9
+
+bfs(graph , 1 , visited)
