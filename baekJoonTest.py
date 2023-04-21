faf40bb2aee955a4270ae69857e62ea20cd9b4eb
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-04-21 11:49:42 +0900

    add : 4.21 정렬 개념 공부

--- baekJoonTest.py
+++ baekJoonTest.py
@@ -1,23 +1,14 @@
-def dfs(graph,v,visited):
-    visited[v] = True
-    print(v, end = ' ')
-    for i in graph[v] :
-        print('index',i)
-        if not visited[i]:
-            dfs(graph, i , visited)
+array = [7,5,9,0,3,1,6,2,9,1,4,8,0,5,2]
 
-graph = [
-    [],
-    [2,3,8],
-    [1,7],
-    [1,4,5],
-    [3,5],
-    [3,4],
-    [7],
-    [2,6,8],
-    [1,7]
-]
+# 가장 큰수 max 함수로 확인 
+count = [0] * (max(array) + 1)
+print(count)
 
-visited = [False] * 9
+# array를 순회하면서 동일한 넘버에 1씩 더한다 
+for i in range(len(array)) :
+    count[array[i]] += 1
 
-dfs(graph , 1 , visited)
+# count의 길이 만큼 순회하고 이중포문을 돌면서 카운트된 갯수만큼 순회하면서 정렬시킨다.
+for i in range(len(count)) :
+    for j in range(count[i]) :
+        print(i,end =" ")
