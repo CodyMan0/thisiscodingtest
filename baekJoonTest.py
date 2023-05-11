ff5f705e44e094d33f62e5d0237e148d35f68d28
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-05-11 10:41:45 +0900

    add : 5.11 bfs/dfs 실전 문제 한문제 및 백준 실버 레벨로 기본적인 문제 반복 숙달

--- baekJoonTest.py
+++ baekJoonTest.py
@@ -1,5 +1,23 @@
-li = [int(input()) for _ in range(5)]
-if li[0] <= 0:
-    print(-li[0]*li[2] + li[3] + li[1]*li[4])
-else:
-    print((li[1]-li[0])*li[4])
+C = int(input())
+N = int(input())
+
+graph = [[] for _ in range(C + 1)]
+visited = [False] * (C + 1)
+
+for i in range(N) : 
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def dfs(graph, v, visited) :
+    visited[v] = True
+    for i in graph[v]:
+        if visited[i] == False:
+            dfs(graph,i,visited)
+
+
+
+print(graph)
+dfs(graph,1,visited)
+print(visited.count(True) -1)
+
