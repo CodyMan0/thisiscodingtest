5347cd13cbedf29131c4bdc528c4a00f5432598b
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-05-30 11:02:58 +0900

    add : 5.30 동적 계획법 문제 풀이

---
+++ 다이나믹프로그래밍/실전문제/금광.py
@@ -0,0 +1,38 @@
+# 다이내믹 프로그래밍은 아직 어떻게 푸는지 모르겠다. 
+## array는 초기 금광 정보 
+## dp 변수는 동적 계획을 위한 2차원 테이블
+
+## dp[i][j] = array[i][j] + max(dp[i-1][j-1], dp[i][j -1] , dp[i + 1][j - 1 ])
+
+for tc in range(int(input())) :
+    n,m = map(int,input().split())
+    array = list(map(int, input().split()))
+
+    dp = [ ]
+    index = 0
+    for i in range(n):
+        dp.append(array[index : index + m])
+        index += m
+
+    for j in range(1, m): 
+        for i in range(n):
+            if i == 0 :
+                left_up = 0
+                
+            else :
+                left_up = dp[i-1][j-1]
+                print('i',i,j)
+            if i == n-1 :
+                left_down = 0
+            else :
+                left_down = dp[i+1][j-1]
+            left = dp[i][j-1]
+            dp[i][j] = dp[i][j] + max(left_up, left_down, left)
+    
+    result = 0
+    for i in range(n):
+        result = max(result, dp[i][m -1])
+    
+    print('res',result)
+
+
--- 이진탐색/실전문제/공유기설치.py
+++ 이진탐색/실전문제/공유기설치.py
@@ -4,7 +4,13 @@
 ## 2. x를 기준으로 공유기를 설치
 ## 3. 공유기를 C개 이상 설치할 수 있다.
 
-
+# 입력 
+# 5 3
+# 1
+# 2
+# 8
+# 4
+# 9
 # 10억이라는 수는 이분 탐색으로 밖에 해결 못하기에 힌트가 될 수 있다. 
 
 n, c = list(map(int,input().split(' ')))
@@ -21,11 +27,15 @@ result = 0
 
 while (start <= end) :
     mid = (start + end) // 2
+    print('e',start,end)
     value = array[0]
     count = 1
     
     for i in range(1, n) :
+        print('i',i)
         if array[i] >= value + mid :
+            # 첫번째와 미드값을 더했을때
+            # array[i]보다 작으면
             value = array[i]
             count += 1
     if count >= c :
