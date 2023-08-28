bb1c8fbb0fb035e48f4dc65837354c59549a9b1f
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-08-28 21:04:01 +0900

    두번 풀기

--- 최단경로/미래도시.py
+++ 최단경로/미래도시.py
@@ -15,6 +15,42 @@
 # 4 5
 
 
+# # 책 풀이
+# INF = int(1e9)
+# n,m = map(int,input().split())
+# graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+# for a in range(1, n + 1) :
+#     for b in range(1, n + 1) :
+#         if a == b :
+#             graph[a][b] == 0
+
+# for _ in range(m) :
+#     a , b = map(int,input().split())
+#     graph[a][b] = 1
+#     graph[b][a] = 1
+
+# x , k = map(int, input().split())
+
+# for k in range(1, n + 1) :
+#     for a in range(1, n + 1):
+#         for b in range(1, n + 1):
+#             graph[a][b] = min(graph[a][b], graph[a][k] + graph[b][k])
+
+# distance = graph[1][k] + graph[k][x]
+
+# if distance >= INF :
+#     print('-1')
+# else:
+#     print(distance)
+    
+
+
+
+# ## 30분 고민하다가 풀이를 보고 이해해보기로...
+
+
+
 # 책 풀이
 INF = int(1e9)
 n,m = map(int,input().split())
