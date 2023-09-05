9d645ab4df262c47933ebecb2a48dcaf5eb497c9
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-09-05 23:59:23 +0900

    정렬 다시 풀기

--- 구현/상하좌우.py
+++ 구현/상하좌우.py
@@ -2,31 +2,7 @@
 # 가장 왼쪽을 (1,1) 오른쪽 아래를 (N,N)이라고 한다. 여행가 A는 상 하 좌 우로
 # 이동 가능하며 시작은 항상 (1,1)이다. 우리 앞에는 여행가 A가 이동할 계획이 적힌 계획서가 놓여 있다. 
 # 계획서에는 하나의 줄에 띄어쓰기를 기준으로 L,R,U,D 중 하나의 문자가 반복적으로 적혀있다. 
-# value = int(input())
-# list = list(map(str,input().split()))
-# x , y = (1,1)
-
-# for way in list :
-    
-#     if way == 'R' :
-#         nx = x
-#         ny = y + 1
-#     elif way == 'L' :
-#         nx = x
-#         ny = y -1
-#     elif way == "U" :
-#         nx = x -1
-#         ny = y
-#     else : 
-#         nx = x + 1
-#         ny = y
-
-
-#     # 0 일때는 상위 스코프에 있는 x , y를 가지고 올 수 있도록 함. 
-#     if 0<nx<=value and 0<ny<=value:
-#         x = nx
-#         y = ny
-# print(x,y)
+# ㅜ
         
 
 # x가 1일때는 U 무시
